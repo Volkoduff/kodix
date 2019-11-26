521f9d0a5d2d25aff90c49c42b8e44cc94265f24
Kovalkin Vladimir <vakovalkin@yandex.ru>
2019-11-26 15:32:22 +0300

    Подключает SCSS

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import 'jquery/dist/jquery.js';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
+import './css/media-queries.css'
+import './scss/main.scss'
 
 import AppController from './components/controllers/app-controller';
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,41 @@
 const path = require(`path`);
+const MiniCssExtractPlugin = require(`mini-css-extract-plugin`);
 
 module.exports = {
-	mode: `development`,
-	entry: `./src/main.js`,
-	output: {
-		filename: `bundle.js`,
-		path: path.join(__dirname, `dist`),
-	},
-	devtool: `source-map`,
-    module: {
-      rules: [
-        {
-          test: /\.css$/i,
-          use: [`style-loader`, `css-loader`],
-        }
-      ]
+    mode: `development`,
+    entry: `./src/main.js`,
+    output: {
+        filename: `bundle.js`,
+        path: path.join(__dirname, `dist`),
     },
-	devServer: {
-		contentBase: path.join(__dirname, `dist`),
-		publicPath: 'http://localhost:8080',
-		compress: true,
-		watchContentBase: true,
-	},
+    devtool: `source-map`,
+    module: {
+        rules: [{
+            test: /\.css$/i,
+            use: [`style-loader`, `css-loader`],
+        },
+            {
+                test: /\.scss$/,
+                use: [`style-loader`, MiniCssExtractPlugin.loader, {
+                    loader: `css-loader`,
+                    options: {sourceMap: true}
+                },
+                {
+                    loader: `sass-loader`,
+                    options: {sourceMap: true}
+                }],
+            }]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, `dist`),
+        publicPath: 'http://localhost:8080',
+        compress: true,
+        watchContentBase: true,
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'main.css',
+            ignoreOrder: false, // Enable to remove warnings about conflicting order
+        }),
+    ],
 };
